96fc84cbf350c78794a94000c9639909521742b9
jaykim <vita.k@kakao.com>
2025-02-21 16:42:35 +0900

    init

---
+++ app/src/main/java/com/example/vita_kotlin/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.vita_kotlin
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.vita_kotlin.ui.theme.VitaKotlinTheme
+
+class MainActivity : ComponentActivity() {
+    lateinit var loginActivityResultLauncher: ActivityResultLauncher<Intent>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val username = intent.getStringExtra("username") ?: "Guest"
+        setContent {
+            VitaKotlinTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(username)
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    VitaKotlinTheme {
+        Greeting("Android")
+    }
+}
